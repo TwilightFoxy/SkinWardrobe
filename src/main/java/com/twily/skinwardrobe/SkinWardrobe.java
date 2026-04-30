c24614866b038dd8b3640e6a9d61f27c78c2d452
twilight <5novel5@mail.ru>
2026-04-30 16:27:06 +0300

    Add NeoForge mod scaffold

---
+++ src/main/java/com/twily/skinwardrobe/SkinWardrobe.java
@@ -0,0 +1,13 @@
+package com.twily.skinwardrobe;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SkinWardrobe.MOD_ID)
+public final class SkinWardrobe {
+    public static final String MOD_ID = "skinwardrobe";
+
+    public SkinWardrobe(IEventBus modBus, ModContainer modContainer) {
+    }
+}
